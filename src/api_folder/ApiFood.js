acfe2006f65138df897d450aa21074cea98a9fde
mauritshdb <unconfigured@null.spigotmc.org>
2023-01-08 17:08:52 +0700

    add if when user not logged in

--- src/api_folder/ApiFood.js
+++ src/api_folder/ApiFood.js
@@ -1,13 +1,25 @@
 import Axios from 'axios';
-
+const isLogin = Boolean(localStorage.getItem("token") || false)
 const getData = () => {
-  return Axios({
-    method: 'get',
-    url: `${process.env.REACT_APP_BASEURL}/api/v1/foods`,
-    headers: {
-      apiKey: `${process.env.REACT_APP_APIKEY}`,
-    }
-  })
+  if (isLogin) {
+    return Axios({
+      method: 'get',
+      url: `${process.env.REACT_APP_BASEURL}/api/v1/foods`,
+      headers: {
+        apiKey: `${process.env.REACT_APP_APIKEY}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      }
+    })
+  } else {
+    return Axios({
+      method: 'get',
+      url: `${process.env.REACT_APP_BASEURL}/api/v1/foods`,
+      headers: {
+        apiKey: `${process.env.REACT_APP_APIKEY}`,
+      }
+    })
+  }
+
 }
 
 export default getData;
