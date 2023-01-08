b4b10f0befcf85732fe1c6ff9727213532046459
mauritshdb <unconfigured@null.spigotmc.org>
2023-01-08 18:13:05 +0700

    add data to api register

--- src/api_folder/ApiRegister.js
+++ src/api_folder/ApiRegister.js
@@ -1,15 +1,20 @@
 import Axios from "axios";
 
-const register = (email, password) => {
+const register = (name, email, password, passwordRepeat, role, profilePictureUrl, phoneNumber) => {
     return Axios({
         method: 'post',
         url: `${process.env.REACT_APP_BASEURL}/api/v1/register`,
         headers: {
             apiKey: `${process.env.REACT_APP_APIKEY}`,
-          },
+        },
         data: {
+            name: name,
             email: email,
-            password: password
+            password: password,
+            passwordRepeat: passwordRepeat,
+            role: role,
+            profilePictureUrl: profilePictureUrl,
+            phoneNumber: phoneNumber
         }
     });
 }
