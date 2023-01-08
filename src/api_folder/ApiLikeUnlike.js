97bbab4cd51792cbc2b39f5f1f5c6c9b40ee475c
mauritshdb <unconfigured@null.spigotmc.org>
2023-01-08 17:07:58 +0700

    logging

--- src/api_folder/ApiLikeUnlike.js
+++ src/api_folder/ApiLikeUnlike.js
@@ -1,14 +1,6 @@
-import Axios from 'axios';
 
-const handlelike = (id, isLike) => {
-    return Axios({
-        method: 'post',
-        url: `${process.env.REACT_APP_BASEURL}/api/v1/like`,
-        headers: {
-            apiKey: `${process.env.REACT_APP_APIKEY}`,
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-        data: { foodId: id },
-    });
+// const isLogin = Boolean(localStorage.getItem("token") || false)
 
-}
+// FAIL CANNOT IMPORT FROM APP
+
+// export default handleLike;
