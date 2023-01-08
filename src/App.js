89650d75ff7f7dfbb1beab94f6591d39948a65e6
mauritshdb <unconfigured@null.spigotmc.org>
2023-01-08 17:08:14 +0700

    move api to main file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import './App.css';
+import './css/App.css';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import { useEffect, useState } from 'react';
 import * as AiIcons from 'react-icons/ai';
 import { IconContext } from "react-icons";
 import getData from './api_folder/ApiFood';
-import getUser from './api_folder/ApiGetUser'
+import getUser from './api_folder/ApiGetUser';
+// import handleLike from './api_folder/ApiLikeUnlike';
+import Axios from 'axios';
+import { Row } from 'react-bootstrap';
 
 
-function App() {
+export default function App() {
   const [food, setFood] = useState([]);
   const [name, setName] = useState();
   const [img, setImg] = useState();
 
   const isLogin = Boolean(localStorage.getItem("token") || false)
 
-  useEffect(() => {
+  const GetFood = () => {
     getData().then(function (res) {
       setFood(res.data.data)
     });
+  }
+
+  const handleLike = (id, isLike) => {
+    if (isLogin) {
+        if (isLike) {
+            return Axios({
+                method: 'post',
+                url: `${process.env.REACT_APP_BASEURL}/api/v1/unlike`,
+                headers: {
+                    apiKey: `${process.env.REACT_APP_APIKEY}`,
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+                data: { foodId: id },
+            }).then(res => {
+                GetFood();
+            })
+        } else {
+            return Axios({
+                method: 'post',
+                url: `${process.env.REACT_APP_BASEURL}/api/v1/like`,
+                headers: {
+                    apiKey: `${process.env.REACT_APP_APIKEY}`,
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+                data: { foodId: id },
+            }).then(res => {
+                GetFood();
+            })
+        }
+    } else {
+        alert('login first');
+        window.location.assign('/login');
+    }
+}
+
+  useEffect(() => {
+    GetFood();
     if (isLogin) {
       getUser().then(function (res) {
         setName(res.data.user.name)
@@ -30,6 +70,9 @@ function App() {
 
   return (
     <>
+    <Row>
+      
+    </Row>
       <div className='zxc'>
         <div className='c1'>
           {food.map((item, index) => {
@@ -48,15 +91,15 @@ function App() {
                     {item.rating}
                   </IconContext.Provider>
                 </Badge>{' '}
-                <Badge pill bg="light" text='dark' className='pillLikes'>
-                  <IconContext.Provider value={{ color: 'red' }}>
+                <Badge pill bg="light" text='dark' className='pillLikes' onClick={() => handleLike(item.id, item.isLike)}>
+                  <IconContext.Provider value={{ color: `${item.isLike ? "red" : "grey"}` }}>
                     <AiIcons.AiFillHeart />
                     {item.totalLikes}
                   </IconContext.Provider>
                 </Badge>{' '}
 
                 <Card.Body>
-                  <Card.Title>{item.name}
+                  <Card.Title>{item.name}{item.isLike}
                   </Card.Title>
                   <Card.Text>
                     {item.description}
@@ -82,13 +125,10 @@ function App() {
             );
           })}
         </div>
-        <img src={img} style={{width: '10%'}} />
-        <h1 className='userr'>{name}</h1>
       </div>
-
+      <img src={img} style={{ width: '10%' }} />
+        <h1 className='userr'>{name}</h1>
 
     </>
   );
 }
-
-export default App;
