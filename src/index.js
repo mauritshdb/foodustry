8fc9bd77c406ce3a488f042224ac827f93ad82f8
mauritshdb <unconfigured@null.spigotmc.org>
2023-01-08 10:44:12 +0700

    add login and change navbar to sidebar

--- src/index.js
+++ src/index.js
@@ -3,35 +3,42 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NavbarTop from './components/TheNavbar';
+import TSidebar from './components/TheSidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarTop from './Components/TheNavbar';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <>
-    <NavbarTop/>
-    <Outlet/>
+      <TSidebar />
+      {/* <NavbarTop /> */}
+      <Outlet />
     </>,
-    errorElement:<center>Page Not Found or something error😥</center>,
-    children:[
+    errorElement: <h1>something error</h1>,
+    children: [
       {
         path: '/',
-        element: <App/>
-      }
+        element: <App />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/logout',
+        element: <Logout />
+      },
     ]
   }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+  // <React.StrictMode>
+    <RouterProvider router={router} />
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
