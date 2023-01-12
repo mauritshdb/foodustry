44bed338a410abf14c24f5c36efe15b530d1beb5
mauritshdb <unconfigured@null.spigotmc.org>
2023-01-13 03:42:22 +0700

    change favorite to sign up when not logged in

--- src/Components/TheSidebarData.jsx
+++ src/Components/TheSidebarData.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as IoIcons from 'react-icons/io';
 import * as IoIcons5 from 'react-icons/io5';
 import * as BiIcons from 'react-icons/bi';
+import * as HiIcons from 'react-icons/hi';
 
 const isLogin = Boolean(localStorage.getItem("token") || false)
 
@@ -9,19 +10,19 @@ export const SidebarData = [
     {
         title: 'Home',
         path: '/',
-        icon: <IoIcons.IoIosHome/>,
+        icon: <IoIcons.IoIosHome />,
         cName: 'nav-text',
     },
     {
         title: isLogin ? 'Sign out' : 'Sign in',
         path: isLogin ? '/logout' : '/login',
-        icon: isLogin ? <BiIcons.BiLogOut/> : <BiIcons.BiLogIn/>,
+        icon: isLogin ? <BiIcons.BiLogOut /> : <BiIcons.BiLogIn />,
         cName: 'nav-text',
     },
     {
-        title: 'Favorites',
-        path: '/favorites',
-        icon: <IoIcons5.IoFastFoodSharp/>,
+        title: !isLogin ? 'Sign up' : 'Favorites',
+        path: !isLogin ? '/register' : '/favorites',
+        icon: !isLogin ? <HiIcons.HiUserAdd/> : <IoIcons5.IoFastFoodSharp />,
         cName: 'nav-text',
     }
 ]
